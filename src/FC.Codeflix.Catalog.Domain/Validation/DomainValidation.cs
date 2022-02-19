3eb64be47037f3778ec493ce76cbc78291a29e70
Wilson Neto <contato@wilsonneto.com.br>
2022-02-19 17:48:08 -0300

    aula 01-02-14 - 22: Criando a DomainValidation NotNull

---
+++ src/FC.Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.Domain.Validation;
+
+public class DomainValidation
+{
+    public static void NotNull(object target, string fieldName)
+    {
+        if (target is null)
+            throw new EntityValidationException(
+                $"{fieldName} should not be null");
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+using System;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Validation;
+
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    // nao ser null
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        var value = Faker.Commerce.ProductName();
+        Action action = 
+            () => DomainValidation.NotNull(value, "Value");
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        string value = null;
+        Action action =
+            () => DomainValidation.NotNull(value, "FieldName");
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+
+    // nao ser null ou vazio
+    // tamanho minimo
+    // tamanho maximo
+}
