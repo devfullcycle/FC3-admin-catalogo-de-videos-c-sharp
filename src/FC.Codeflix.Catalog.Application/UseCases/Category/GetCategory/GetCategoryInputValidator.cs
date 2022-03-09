66262d0c70cf6117faee8554c667d12089be770a
Wilson Neto <contato@wilsonneto.com.br>
2022-03-09 08:06:56 -0300

    Aula 03-16: Validação do input do GetCategory

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryInputValidator 
+    : AbstractValidator<GetCategoryInput>
+{
+    public GetCategoryInputValidator()
+        => RuleFor(x => x.Id).NotEmpty();
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidatorTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+}
