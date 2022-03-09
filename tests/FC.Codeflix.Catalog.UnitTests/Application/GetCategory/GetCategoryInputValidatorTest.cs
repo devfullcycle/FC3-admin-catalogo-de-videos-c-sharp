a293679dbff45c98a3452ceef4da1687137fe9ee
Wilson Neto <contato@wilsonneto.com.br>
2022-03-09 08:14:06 -0300

    Aula 17: Ultímo teste do validator do input do GetCategory

--- tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -26,4 +26,20 @@ public class GetCategoryInputValidatorTest
         validationResult.IsValid.Should().BeTrue();
         validationResult.Errors.Should().HaveCount(0);
     }
+
+    [Fact(DisplayName = nameof(InvalidWhenEmptyGuidId))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void InvalidWhenEmptyGuidId()
+    {
+        var invalidInput = new GetCategoryInput(Guid.Empty);
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(invalidInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors[0].ErrorMessage
+            .Should().Be("'Id' must not be empty.");
+    }
 }
