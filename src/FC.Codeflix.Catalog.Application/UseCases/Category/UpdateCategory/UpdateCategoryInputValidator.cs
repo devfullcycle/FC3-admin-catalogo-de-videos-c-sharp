3ede360b01198ca0367d51711f06a82c6dc342c4
Wilson Neto <contato@wilsonneto.com.br>
2022-03-11 02:24:00 -0300

    Aula 03-26: Validation do UpdateCategoryInput

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInputValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+public class UpdateCategoryInputValidator
+    : AbstractValidator<UpdateCategoryInput>
+{
+    public UpdateCategoryInputValidator()
+        => RuleFor(x => x.Id).NotEmpty();
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryInputValidatorTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Application", "UpdateCategoryInputValidator - Use Cases")]
+    public void DontValidateWhenEmptyGuid()
+    {
+        var input = _fixture.GetValidInput(Guid.Empty);
+        var validator = new UpdateCategoryInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeFalse();
+        validateResult.Errors.Should().HaveCount(1);
+        validateResult.Errors[0].ErrorMessage
+            .Should().Be("'Id' must not be empty.");
+    }
+
+
+    [Fact(DisplayName = nameof(ValidateWhenValid))]
+    [Trait("Application", "UpdateCategoryInputValidator - Use Cases")]
+    public void ValidateWhenValid()
+    {
+        var input = _fixture.GetValidInput();
+        var validator = new UpdateCategoryInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeTrue();
+        validateResult.Errors.Should().HaveCount(0);
+    }
+}
