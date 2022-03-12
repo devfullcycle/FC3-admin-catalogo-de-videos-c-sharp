57540fd01f7369840a36ee553ac89a9f95e297f9
Wilson Neto <contato@wilsonneto.com.br>
2022-03-11 21:11:26 -0300

    Aula 03-28: Asserts do teste do use case ListQuotation

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,77 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    public async Task List()
+    {
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = new ListCategoriesInput(
+            page: 2,
+            perPage: 15,
+            search: "search-example",
+            sort: "name",
+            dir: SearchOrder.Asc
+        );
+        var outputRepositorySearch = new OutputSearch<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            Items: (IReadOnlyList<Category>)categoriesExampleList,
+            Total: 70
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                serachInput.Page == input.Page
+                && serachInput.PerPage == input.PerPage
+                && serachInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        )).RetursAsync(outputRepositorySearch);
+        var useCase = new ListCategories(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(outputRepositorySearch.currentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Total.Should().Be(outputRepositorySearch.Total);
+        output.Items.Should().HaveCount(outputRepositorySearch.Items.Count);
+        output.Items.Foreach(outputItem =>
+        {
+            var repositoryCategory = outputRepositorySearch.Items
+                .Find(x => x.Id == outputItem.Id);
+            outputItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(repositoryCategory.Name);
+            outputItem.Description.Should().Be(repositoryCategory.Description);
+            outputItem.IsActive.Should().Be(repositoryCategory.IsActive);
+            outputItem.CreatedAt.Should().Be(repositoryCategory.CreatedAt);
+        });
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(
+                serachInput.Page == input.Page
+                && serachInput.PerPage == input.PerPage
+                && serachInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,58 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection
+    : ICollectionFixture<ListCategoriesTestFixture>
+{ }
+
+public class ListCategoriesTestFixture
+    : BaseFixture
+{
+    public Mock<ICategoryRepository> GetRepositoryMock()
+        => new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public bool getRandomBoolean()
+        => (new Random()).NextDouble() < 0.5;
+
+    public Category GetExampleCategory()
+        => new Category(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            getRandomBoolean());
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+    {
+        var list = new List<Category>();
+        for (int i = 0; i < length; i++)
+            list.Add(GetExampleCategory());
+        return list;
+    }
+}
