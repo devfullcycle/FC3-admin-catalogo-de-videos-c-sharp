c5b7c56742e265e6ef4819726a861f343dba9da6
Wilson Neto <contato@wilsonneto.com.br>
2022-03-12 11:09:25 -0300

    Aula 03-32: Teste e implementação de resultado vazio na listagem de categories

--- tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -76,6 +76,50 @@ public class ListCategoriesTest
     }
 
 
+    [Fact(DisplayName = nameof(ListOkWhenEmpty))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    public async Task ListOkWhenEmpty()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = _fixture.GetExampleInput();
+        var outputRepositorySearch = new SearchOutput<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: (new List<Category>()).AsReadOnly(),
+            total: 0
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput => searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(outputRepositorySearch);
+        var useCase = new UseCase.ListCategories(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(outputRepositorySearch.CurrentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Total.Should().Be(0);
+        output.Items.Should().HaveCount(0);
+
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput => searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+    }
+
     [Theory(DisplayName = nameof(ListInputWithoutAllParameters))]
     [Trait("Application", "ListCategories - Use Cases")]
     [MemberData(
@@ -136,3 +180,4 @@ public class ListCategoriesTest
         ), Times.Once);
     }
 }
+
