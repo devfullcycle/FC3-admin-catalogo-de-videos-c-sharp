5d637bb0ce3c019d18a5d2c516cf47b0bc7a90f7
Wilson Neto <contato@wilsonneto.com.br>
2022-03-14 22:45:58 -0300

    Aula 04.04: Criando o DbContexto e o mapeamentos da Category

---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/CodeflixCatalogDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF;
+public class CodeflixCatalogDbContext
+    : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public CodeflixCatalogDbContext(
+        DbContextOptions<CodeflixCatalogDbContext> options
+    ) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new CategoryConfiguration());
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
+internal class CategoryConfiguration
+    : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(category => category.Id);
+        builder.Property(category => category.Name)
+            .HasMaxLength(255);
+        builder.Property(category => category.Description)
+            .HasMaxLength(10_000);
+    }
+}
