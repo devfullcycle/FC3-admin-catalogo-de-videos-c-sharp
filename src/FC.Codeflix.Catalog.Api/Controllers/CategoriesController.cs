54cd8799ee5bbb306212a3d2f27ad1398ee8ce0a
Wilson Neto <wn_br@hotmail.com>
2022-04-05 21:20:00 -0300

    Aula 05.18: Implementando endpoint update category

--- src/FC.Codeflix.Catalog.Api/Controllers/CategoriesController.cs
+++ src/FC.Codeflix.Catalog.Api/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@ using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
 using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 using FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
 using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,10 +27,22 @@ public class CategoriesController : ControllerBase
     {
         var output = await _mediator.Send(input, cancellationToken);
         return CreatedAtAction(
-            nameof(Create), 
-            new { output.Id }, 
+            nameof(Create),
+            new { output.Id },
             output
         );
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(
+        [FromBody] UpdateCategoryInput input,
+        CancellationToken cancellationToken
+    )
+    {
+        var output = await _mediator.Send(input, cancellationToken);
+        return Ok(output);
     }
 
     [HttpGet("{id:guid}")]
--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -1,6 +1,9 @@
 ﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net;
 using Xunit;
 
@@ -41,5 +44,94 @@ public class UpdateCategoryApiTest
         dbCategory!.Name.Should().Be(input.Name);
         dbCategory.Description.Should().Be(input.Description);
         dbCategory.IsActive.Should().Be((bool) input.IsActive!);
+    }
+
+    [Fact(DisplayName = nameof(UpdateCategoryOnlyName))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategoryOnlyName()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = new UpdateCategoryInput(
+            exampleCategory.Id, 
+            _fixture.GetValidCategoryName()
+        );
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutput>(
+            $"/categories/{exampleCategory.Id}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Id.Should().Be(exampleCategory.Id);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        var dbCategory = await _fixture
+            .Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+    }
+
+    [Fact(DisplayName = nameof(UpdateCategoryNameAndDescription))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategoryNameAndDescription()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = new UpdateCategoryInput(
+            exampleCategory.Id,
+            _fixture.GetValidCategoryName(),
+            _fixture.GetValidCategoryDescription()
+        );
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutput>(
+            $"/categories/{exampleCategory.Id}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Id.Should().Be(exampleCategory.Id);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        var dbCategory = await _fixture
+            .Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+    }
+
+
+    [Fact(DisplayName = nameof(ErrorWhenNotFound))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void ErrorWhenNotFound()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var randomGuid = Guid.NewGuid();
+        var input = _fixture.GetExampleInput(randomGuid);
+
+        var (response, output) = await _fixture.ApiClient.Put<ProblemDetails>(
+            $"/categories/{randomGuid}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Title.Should().Be("Not Found");
+        output.Type.Should().Be("NotFound");
+        output.Status.Should().Be((int) StatusCodes.Status404NotFound);
+        output.Detail.Should().Be($"Category '{randomGuid}' not found.");
     }
 }
