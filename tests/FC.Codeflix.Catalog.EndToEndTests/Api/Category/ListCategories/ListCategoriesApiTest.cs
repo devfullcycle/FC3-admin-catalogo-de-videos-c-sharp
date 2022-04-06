293ef56c0998cd9be89c1749df2c66c55f02e0fe
Wilson Neto <wn_br@hotmail.com>
2022-04-05 22:43:07 -0300

    Aula 05.20: Primeiro teste do endpoint de listagem de categorias

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTest.cs
@@ -0,0 +1,47 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Net;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.ListCategories;
+
+[Collection(nameof(ListCategoriesApiTestFixture))]
+public class ListCategoriesApiTest
+{
+    private readonly ListCategoriesApiTestFixture _fixture;
+
+    public ListCategoriesApiTest(ListCategoriesApiTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ListCategoriesAndTotalByDefault))]
+    [Trait("EndToEnd/API", "Category/List - Endpoints")]
+
+    public async void ListCategoriesAndTotalByDefault()
+    {
+        var defaultPerPage = 15;
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+
+        var (response, output) = await _fixture.ApiClient
+            .Get<ListCategoriesOutput>("/categories");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Total.Should().Be(exampleCategoriesList.Count);
+        output.Items.Should().HaveCount(defaultPerPage);
+        foreach (CategoryModelOutput outputItem in output.Items)
+        {
+            var exampleItem = exampleCategoriesList
+                .FirstOrDefault(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name); 
+            outputItem.Description.Should().Be(exampleItem.Description);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+        }
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.EndToEndTests.Api.Category.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesApiTestFixture))]
+public class ListCategoriesApiTestFixtureCollection
+    : ICollectionFixture<ListCategoriesApiTestFixture>
+{ }
+
+public class ListCategoriesApiTestFixture
+    : CategoryBaseFixture
+{ }
