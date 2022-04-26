a1d74f63ea7b52b8e041763dd3421853edd9298c
Wilson Neto <wn_br@hotmail.com>
2022-04-25 22:55:49 -0300

    Aula 07.10: Casos de exception do UpdateGenre

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/UpdateGenre/UpdateGenreTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/UpdateGenre/UpdateGenreTest.cs
@@ -7,6 +7,8 @@ using System;
 using System.Threading;
 using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Domain.Exceptions;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Genre.UpdateGenre;
 
@@ -62,5 +64,70 @@ public class UpdateGenreTest
             x => x.Commit(It.IsAny<CancellationToken>()),
             Times.Once
         );
+    }
+
+    [Fact(DisplayName = nameof(ThrowWhenNotFound))]
+    [Trait("Application", "UpdateGenre - Use Cases")]
+    public async Task ThrowWhenNotFound()
+    {
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+        var exampleId = Guid.NewGuid();
+        genreRepositoryMock.Setup(x => x.Get(
+            It.IsAny<Guid>(),
+            It.IsAny<CancellationToken>()
+        )).ThrowsAsync(new NotFoundException(
+            $"Genre '{exampleId}' not found."
+        ));
+        var useCase = new UseCase.UpdateGenre(
+            genreRepositoryMock.Object,
+            _fixture.GetUnitOfWorkMock().Object,
+            _fixture.GetCategoryRepositoryMock().Object
+        );
+        var input = new UseCase.UpdateGenreInput(
+            exampleId,
+            _fixture.GetValidGenreName(),
+            true
+        );
+
+        var action = async () 
+            => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<NotFoundException>()
+            .WithMessage($"Genre '{exampleId}' not found.");
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenNameIsInvalid))]
+    [Trait("Application", "UpdateGenre - Use Cases")]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    public async Task ThrowWhenNameIsInvalid(string? name)
+    {
+
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var exampleGenre = _fixture.GetExampleGenre();
+        var newNameExample = _fixture.GetValidGenreName();
+        var newIsActive = !exampleGenre.IsActive;
+        genreRepositoryMock.Setup(x => x.Get(
+            It.Is<Guid>(x => x == exampleGenre.Id),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(exampleGenre);
+        var useCase = new UseCase.UpdateGenre(
+            genreRepositoryMock.Object,
+            unitOfWorkMock.Object,
+            _fixture.GetCategoryRepositoryMock().Object
+        );
+        var input = new UseCase.UpdateGenreInput(
+            exampleGenre.Id,
+            name!,
+            newIsActive
+        );
+
+        var action = async ()
+            => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<EntityValidationException>()
+            .WithMessage($"Name should not be empty or null");
     }
 }
