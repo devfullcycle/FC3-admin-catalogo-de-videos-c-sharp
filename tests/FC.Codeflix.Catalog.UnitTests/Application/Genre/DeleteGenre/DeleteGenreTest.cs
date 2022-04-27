8c007fb80393d838fa584655b0493a44293997c9
Wilson Neto <wn_br@hotmail.com>
2022-04-26 23:05:25 -0300

    Aula 07.18: Teste do DeleteCategory

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.DeleteGenre;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Genre.DeleteGenre;
+
+[Collection(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTest
+{
+    private readonly DeleteGenreTestFixture _fixture;
+
+    public DeleteGenreTest(DeleteGenreTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteGenre))]
+    [Trait("Application", "DeleteGenre - Use Cases")]
+    public async Task DeleteGenre()
+    {
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var exampleGenre = _fixture.GetExampleGenre();
+        genreRepositoryMock.Setup(x => x.Get(
+            It.Is<Guid>(x => x == exampleGenre.Id),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(exampleGenre);
+        var useCase = new UseCase.DeleteGenre(
+            genreRepositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+        var input = new UseCase.DeleteGenreInput(
+            exampleGenre.Id
+        );
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        genreRepositoryMock.Verify(
+            x => x.Get(
+                It.Is<Guid>(x => x == exampleGenre.Id),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+        genreRepositoryMock.Verify(
+            x => x.Delete(
+                It.Is<DomainEntity.Genre>(x => x.Id == exampleGenre.Id),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+        unitOfWorkMock.Verify(
+            x => x.Commit(It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/DeleteGenre/DeleteGenreTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Application.Genre.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Genre.DeleteGenre;
+
+[CollectionDefinition(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTestFixtureCollection
+    : ICollectionFixture<DeleteGenreTestFixture>
+{ }
+
+public class DeleteGenreTestFixture
+    : GenreUseCasesBaseFixture
+{ }
