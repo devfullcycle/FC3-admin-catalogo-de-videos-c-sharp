f62d90ab1e3e5c675b1292848c2ecddf9727246e
Wilson Neto <wn_br@hotmail.com>
2022-04-29 19:39:32 -0300

    Aula 07.23: ListGenre deve retornar uma lista vazia quando o repositorio retorna vazio

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/ListGenres/ListGenresTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/ListGenres/ListGenresTest.cs
@@ -76,4 +76,45 @@ public class ListGenresTest
             Times.Once
         );
     }
+
+    [Fact(DisplayName = nameof(ListEmpty))]
+    [Trait("Application", "ListGenres - Use Cases")]
+    public async Task ListEmpty()
+    {
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+        var input = _fixture.GetExampleInput();
+        var outputRepositorySearch = new SearchOutput<DomainEntity.Genre>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: (IReadOnlyList<DomainEntity.Genre>) new List<DomainEntity.Genre>(),
+            total: new Random().Next(50, 200)
+        );
+        genreRepositoryMock.Setup(x => x.Search(
+            It.IsAny<SearchInput>(),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(outputRepositorySearch);
+        var useCase = new UseCase
+            .ListGenres(genreRepositoryMock.Object);
+
+        UseCase.ListGenresOutput output =
+            await useCase.Handle(input, CancellationToken.None);
+
+        output.Page.Should().Be(outputRepositorySearch.CurrentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Total.Should().Be(outputRepositorySearch.Total);
+        output.Items.Should().HaveCount(outputRepositorySearch.Items.Count);
+        genreRepositoryMock.Verify(
+            x => x.Search(
+                It.Is<SearchInput>(searchInput =>
+                    searchInput.Page == input.Page
+                    && searchInput.PerPage == input.PerPage
+                    && searchInput.Search == input.Search
+                    && searchInput.OrderBy == input.Sort
+                    && searchInput.Order == input.Dir
+                ),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+    }
 }
