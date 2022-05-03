b2d04405a2625d3a8337182a3e5fb9e7b276d2f5
Wilson Neto <wn_br@hotmail.com>
2022-05-02 23:08:37 -0300

    Aula 08.02: Teste do Insert do GenreRepository

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTest.cs
@@ -0,0 +1,54 @@
+﻿using FC.Codeflix.Catalog.Infra.Data.EF;
+using FluentAssertions;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Repository = FC.Codeflix.Catalog.Infra.Data.EF.Repositories.GenreRepository;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
+
+[Collection(nameof(GenreRepositoryTestFixture))]
+public class GenreRepositoryTest
+{
+    private readonly GenreRepositoryTestFixture _fixture;
+
+    public GenreRepositoryTest(GenreRepositoryTestFixture fixture) 
+        => _fixture = fixture;
+
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "GenreRepository - Repositories")]
+    public async Task Insert()
+    {
+        CodeflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleGenre = _fixture.GetExampleGenre();
+        var categoriesListExample = _fixture.GetExampleCategoriesList(3);
+        categoriesListExample.ForEach(
+            category => exampleGenre.AddCategory(category.Id)
+        );
+        await dbContext.Categories.AddRangeAsync(categoriesListExample);
+        var genreRepository = new Repository.GenreRepository(dbContext);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        await genreRepository.Insert(exampleGenre, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var assertsDbContext = _fixture.CreateDbContext(true);
+        var dbCategory = await assertsDbContext
+            .Categories.FindAsync(exampleGenre.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(exampleGenre.Name);
+        dbCategory.IsActive.Should().Be(exampleGenre.IsActive);
+        dbCategory.CreatedAt.Should().Be(exampleGenre.CreatedAt);
+        var genreCategoriesRelations = await assertsDbContext
+            .GenresCategories.Where(r => r.GenreId == exampleGenre.Id)
+            .ToList();
+        GenresCategories.Shoud().HaveCount(categoriesListExample.Count);
+        genreCategoriesRelations.ForEach(relation => {
+            var expectedCategory = categoriesListExample
+                .FirstOrDefault(x => x.Id == relation.CategoryId);
+            expectedCategory.Should().NotBeNull();
+        });
+    }
+
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTestFixture.cs
@@ -0,0 +1,69 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
+
+[CollectionDefinition(nameof(GenreRepositoryTestFixture))]
+public class GenreRepositoryTestFixtureCollection
+    : ICollectionFixture<GenreRepositoryTestFixture>
+{ }
+
+public class GenreRepositoryTestFixture
+    : BaseFixture
+{
+    public string GetValidGenreName()
+        => Faker.Commerce.Categories(1)[0];
+
+    public bool GetRandomBoolean()
+    => new Random().NextDouble() < 0.5;
+
+    public DomainEntity.Genre GetExampleGenre(
+        bool? isActive = null,
+        List<Guid>? categoriesIds = null
+    )
+    {
+        var genre = new DomainEntity.Genre(
+            GetValidGenreName(),
+            isActive ?? GetRandomBoolean()
+        );
+        categoriesIds?.ForEach(genre.AddCategory);
+        return genre;
+    }
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+        => Enumerable.Range(1, length)
+            .Select(_ => GetExampleCategory()).ToList();
+
+}
