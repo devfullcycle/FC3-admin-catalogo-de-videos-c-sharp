3252153c7537d86a6ca61a0f2e0485365c5fe3ae
Wilson Neto <wn_br@hotmail.com>
2022-05-15 18:50:26 -0300

    Aula 08.15: Implementando o retorno do relacionamento no search do GenreRepository

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/GenreRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/GenreRepository.cs
@@ -67,6 +67,20 @@ public class GenreRepository
     public async Task<SearchOutput<Genre>> Search(SearchInput input, CancellationToken cancellationToken)
     {
         var genres = await _genres.ToListAsync();
+
+        var genresIds = genres.Select(genre => genre.Id).ToList();
+        var relations = await _genresCategories
+            .Where(relation => genresIds.Contains(relation.GenreId))
+            .ToListAsync();
+        var relationsByGenreIdGroup = 
+            relations.GroupBy(x => x.GenreId).ToList();
+        relationsByGenreIdGroup.ForEach(relationGroup => {
+            var genre = genres.Find(genre => genre.Id == relationGroup.Key);
+            if (genre is null) return;
+            relationGroup.ToList()
+                .ForEach(relation => genre.AddCategory(relation.CategoryId));
+        });
+        
         return new SearchOutput<Genre>(
             input.Page,
             input.PerPage,
