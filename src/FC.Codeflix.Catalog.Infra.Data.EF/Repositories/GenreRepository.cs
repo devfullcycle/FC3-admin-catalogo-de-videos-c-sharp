620e82d04830e6fe27d0c8f42afc0cd7869c3a10
Wilson Neto <wn_br@hotmail.com>
2022-05-14 21:25:14 -0300

    08.13: Implementando o Search básico do GenreRepository

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/GenreRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/GenreRepository.cs
@@ -64,9 +64,15 @@ public class GenreRepository
         return Task.CompletedTask;
     }
 
-    public Task<SearchOutput<Genre>> Search(SearchInput input, CancellationToken cancellationToken)
+    public async Task<SearchOutput<Genre>> Search(SearchInput input, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var genres = await _genres.ToListAsync();
+        return new SearchOutput<Genre>(
+            input.Page,
+            input.PerPage,
+            genres.Count,
+            genres
+        );
     }
 
     public async Task Update(Genre genre, CancellationToken cancellationToken)
