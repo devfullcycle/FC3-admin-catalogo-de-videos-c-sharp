a297161bc2dac370b16a012f196807ce675e3b91
Wilson Neto <wn_br@hotmail.com>
2022-05-22 22:01:40 -0300

    Aula 08.25: Teste integrado do use case GetGenre quando há o item

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -1,7 +1,36 @@
 ﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Commom;
 public class GenreUseCasesBaseFixture 
     : BaseFixture
 {
+    public string GetValidGenreName()
+        => Faker.Commerce.Categories(1)[0];
+
+    public bool GetRandomBoolean()
+    => new Random().NextDouble() < 0.5;
+
+    public DomainEntity.Genre GetExampleGenre(
+        bool? isActive = null,
+        List<Guid>? categoriesIds = null,
+        string? name = null
+    )
+    {
+        var genre = new DomainEntity.Genre(
+            name ?? GetValidGenreName(),
+            isActive ?? GetRandomBoolean()
+        );
+        categoriesIds?.ForEach(genre.AddCategory);
+        return genre;
+    }
+
+    public List<DomainEntity.Genre> GetExampleListGenres(int count = 10)
+        => Enumerable
+            .Range(1, count)
+            .Select(_ => GetExampleGenre())
+            .ToList();
 }
--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -1,4 +1,9 @@
-﻿using Xunit;
+﻿using UseCase = FC.Codeflix.Catalog.Application.UseCases.Genre.GetGenre;
+using System.Threading.Tasks;
+using Xunit;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using System.Threading;
+using FluentAssertions;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.GetGenre;
 
@@ -9,4 +14,26 @@ public class GetGenreTest
 
     public GetGenreTest(GetGenreTestFixture fixture) 
         => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(GetGenre))]
+    [Trait("Integration/Application", "GetGenre - Use Cases")]
+    public async Task GetGenre()
+    {
+        var genresExampleList = _fixture.GetExampleListGenres(10);
+        var expectedGenre = genresExampleList[5];
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genresExampleList);
+        await dbArrangeContext.SaveChangesAsync();
+        var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
+        var useCase = new UseCase.GetGenre(genreRepository);
+        var input = new UseCase.GetGenreInput(expectedGenre.Id);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(expectedGenre.Id);
+        output.Name.Should().Be(expectedGenre.Name);
+        output.IsActive.Should().Be(expectedGenre.IsActive);
+        output.CreatedAt.Should().Be(expectedGenre.CreatedAt);
+    }
 }
