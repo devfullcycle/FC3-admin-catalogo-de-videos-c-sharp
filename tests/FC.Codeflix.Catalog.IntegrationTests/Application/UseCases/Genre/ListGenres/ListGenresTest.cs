ee0b3baad8c376bfd2df571c6cf80bc9ca52a124
Wilson Neto <wn_br@hotmail.com>
2022-05-29 20:49:19 -0300

    Aula 08.40: Teste integrado ListGenres

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/ListGenres/ListGenresTest.cs
@@ -0,0 +1,53 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Genre.ListGenres;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Genre.ListGenres;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.ListGenres;
+
+[Collection(nameof(ListGenresTestFixture))]
+public class ListGenresTest
+{
+    private readonly ListGenresTestFixture _fixture;
+
+    public ListGenresTest(ListGenresTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ListGenres))]
+    [Trait("Integration/Application", "ListGenres - UseCases")]
+    public async Task ListGenres()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        CodeflixCatalogDbContext arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(exampleGenres);
+        await arrangeDbContext.SaveChangesAsync();
+        UseCase.ListGenres useCase = new UseCase.ListGenres(
+            new GenreRepository(_fixture.CreateDbContext(true))
+        );
+        UseCase.ListGenresInput input = new UseCase.ListGenresInput(1, 20);
+
+        ListGenresOutput output = await useCase.Handle(
+            input, 
+            CancellationToken.None
+        );
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Items.Should().HaveCount(exampleGenres.Count);
+        output.Items.ToList().ForEach(outputItem => {
+            DomainEntity.Genre? exampleItem = 
+                exampleGenres.Find(example => example.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+        });
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/ListGenres/ListGenresTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Commom;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.ListGenres;
+
+[CollectionDefinition(nameof(ListGenresTestFixture))]
+public class ListGenresTestFixtureCollection
+    : ICollectionFixture<ListGenresTestFixture>
+{ }
+
+public class ListGenresTestFixture
+    : GenreUseCasesBaseFixture
+{
+
+}
