b17e456906f76d0a020c1021098bdea58bf37422
Wilson Neto <wn_br@hotmail.com>
2022-05-29 19:54:21 -0300

    Aula 08.37: Teste integrado delete Genre

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -0,0 +1,41 @@
+﻿using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
+using System.Threading.Tasks;
+using Xunit;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using System.Threading;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.DeleteGenre;
+
+[Collection(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTest
+{
+    private readonly DeleteGenreTestFixture _fixture;
+
+    public DeleteGenreTest(DeleteGenreTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteGenre))]
+    [Trait("Integration/Application", "DeleteGenre - Use Cases")]
+    public async Task DeleteGenre()
+    {
+        var genresExampleList = _fixture.GetExampleListGenres(10);
+        var targetGenre = genresExampleList[5];
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genresExampleList);
+        await dbArrangeContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(
+            new GenreRepository(actDbContext),
+            new UnitOfWork(actDbContext)
+        );
+        var input = new UseCase.DeleteGenreInput(targetGenre.Id);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
+        genreFromDb.Should().BeNull();
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTestFixture.cs
@@ -0,0 +1,15 @@
+﻿
+using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Commom;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.DeleteGenre;
+
+[CollectionDefinition(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTestFixtureCollection
+    : ICollectionFixture<DeleteGenreTestFixture>
+{ }
+
+public class DeleteGenreTestFixture
+    : GenreUseCasesBaseFixture
+{
+}
