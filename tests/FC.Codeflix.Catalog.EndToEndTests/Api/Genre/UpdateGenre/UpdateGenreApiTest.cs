7469cf6dd6a7e3d7ca1c27cf21ecbb8050c904cb
Wilson Neto <wn_br@hotmail.com>
2022-06-12 20:17:42 -0300

    Aula 09.15: Teste do endpoint de update

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/UpdateGenre/UpdateGenreApiTest.cs
@@ -0,0 +1,50 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Api.ApiModels.Genre;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.UpdateGenre;
+
+[Collection(nameof(UpdateGenreApiTestFixture))]
+public class UpdateGenreApiTest
+{
+    private readonly UpdateGenreApiTestFixture _fixture;
+
+    public UpdateGenreApiTest(UpdateGenreApiTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(UpdateGenre))]
+    [Trait("EndToEnd/Api", "Genre/UpdateGenre - Endpoints")]
+    public async Task UpdateGenre()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var targetGenre = exampleGenres[5];
+        await _fixture.Persistence.InsertList(exampleGenres);
+        var input = new UpdateGenreApiInput(
+            _fixture.GetValidGenreName(), 
+            _fixture.GetRandomBoolean()
+        );
+
+        var (response, output) = await _fixture.ApiClient
+            .Put<ApiResponse<GenreModelOutput>>(
+                $"/genres/{targetGenre.Id}",
+                input
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Data.Id.Should().Be(targetGenre.Id);
+        output.Data.Name.Should().Be(input.Name);
+        output.Data.IsActive.Should().Be(input.IsActive);
+        var genreFromDb = await _fixture.Persistence.GetById(output.Data.Id);
+        genreFromDb.Should().NotBeNull();
+        genreFromDb!.Name.Should().Be(input.Name);
+        genreFromDb.IsActive.Should().Be(input.IsActive);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/UpdateGenre/UpdateGenreApiTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using FC.Codeflix.Catalog.EndToEndTests.Api.Genre.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.UpdateGenre;
+
+[CollectionDefinition(nameof(UpdateGenreApiTestFixture))]
+public class UpdateGenreApiTestFixtureCollection
+    : ICollectionFixture<UpdateGenreApiTestFixture>
+{}
+
+public class UpdateGenreApiTestFixture
+    : GenreBaseFixture
+{
+}
