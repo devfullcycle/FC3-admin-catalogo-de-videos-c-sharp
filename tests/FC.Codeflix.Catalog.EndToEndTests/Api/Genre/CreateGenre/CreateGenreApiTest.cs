b5a7500cbd54c646e5ac8c235ffa2d3f82bb2503
Wilson Neto <wn_br@hotmail.com>
2022-06-12 17:31:09 -0300

    Aula 09.11: Teste do endpoint de create

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/CreateGenre/CreateGenreApiTest.cs
@@ -0,0 +1,45 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.CreateGenre;
+
+[Collection(nameof(CreateGenreApiTestFixture))]
+public class CreateGenreApiTest
+{
+    private readonly CreateGenreApiTestFixture _fixture;
+
+    public CreateGenreApiTest(CreateGenreApiTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(CreateGenre))]
+    [Trait("EndToEnd/Api", "Genre/CreateGenre - Endpoints")]
+    public async Task CreateGenre()
+    {
+        var apiInput = new CreateGenreInput(
+            _fixture.GetValidCategoryName(),
+            _fixture.GetRandomBoolean()
+        );
+
+        var (response, output) = await _fixture.ApiClient
+            .Post<ApiResponse<GenreModelOutput>>($"/genres", apiInput);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status201Created);
+        output.Should().NotBeNull();
+        output!.Data.Should().NotBeNull();
+        output.Data.Id.Should().NotBeEmpty();
+        output.Data.Name.Should().Be(apiInput.Name);
+        output.Data.IsActive.Should().Be(apiInput.IsActive);
+        output.Data.Categories.Should().HaveCount(0);
+        var genreFromDb = await _fixture.Persistence.GetById(output.Data.Id);
+        genreFromDb.Should().NotBeNull();
+        genreFromDb!.Name.Should().Be(apiInput.Name);
+        genreFromDb.IsActive.Should().Be(apiInput.IsActive);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/CreateGenre/CreateGenreApiTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using FC.Codeflix.Catalog.EndToEndTests.Api.Genre.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.CreateGenre;
+
+[CollectionDefinition(nameof(CreateGenreApiTestFixture))]
+public class CreateGenreApiTestFixtureCollection
+    : ICollectionFixture<CreateGenreApiTestFixture>
+{ }
+
+public class CreateGenreApiTestFixture
+    : GenreBaseFixture
+{
+}
