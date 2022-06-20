9ff0408cfdc9b7fe5c34cb2a7a059b4e5faa84a7
Wilson Neto <wn_br@hotmail.com>
2022-06-19 22:26:28 -0300

    Aula 09.21: Criando o primeiro teste do ListGenres

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/ListGenres/ListGenresApiTest.cs
@@ -0,0 +1,54 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.ListGenres;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.ListGenres;
+
+[Collection(nameof(ListGenresApiTestFixture))]
+public class ListGenresApiTest
+{
+    private readonly ListGenresApiTestFixture _fixture;
+
+    public ListGenresApiTest(ListGenresApiTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("EndToEnd/Api", "Genre/ListGenres - Endpoints")]
+    public async Task List()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        await _fixture.Persistence.InsertList(exampleGenres);
+        var input = new ListGenresInput();
+        input.Page = 1;
+        input.PerPage = exampleGenres.Count;
+
+        var (response, output) = await _fixture.ApiClient
+            .Get<ApiResponse<ListGenresOutput>>("/genres", input);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Data.Total.Should().Be(exampleGenres.Count);
+        output.Data.Items.Count.Should().Be(exampleGenres.Count);
+        output.Data.Page.Should().Be(input.Page);
+        output.Data.PerPage.Should().Be(input.PerPage);
+        output.Data.Items.ToList().ForEach(outputItem =>
+        {
+            var exampleItem = exampleGenres.Find(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+
+        });
+
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/ListGenres/ListGenresApiTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using FC.Codeflix.Catalog.EndToEndTests.Api.Genre.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.ListGenres;
+
+[CollectionDefinition(nameof(ListGenresApiTestFixture))]
+public class ListGenresApiTestFixtureCiollection
+    : ICollectionFixture<ListGenresApiTestFixture>
+{ }
+
+public class ListGenresApiTestFixture
+    : GenreBaseFixture
+{
+}
