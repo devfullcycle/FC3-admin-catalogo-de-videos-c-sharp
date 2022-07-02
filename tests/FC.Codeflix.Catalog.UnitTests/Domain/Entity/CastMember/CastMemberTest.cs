3b88b146090c4b05238d169ce830b7c287ef1542
Wilson Neto <wn_br@hotmail.com>
2022-07-02 08:58:49 -0300

    Aula 10.02: Iniciando o teste da entidade e raiz de agregação CastMember

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/CastMember/CastMemberTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Entity = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Enum;
+using FluentAssertions;
+using System;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.CastMember;
+
+[Collection(nameof(CastMemberTestFixture))]
+public class CastMemberTest
+{
+    private CastMemberTestFixture _fixture;
+
+    public CastMemberTest(CastMemberTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "CastMember - Aggregates")]
+    public void Instantiate()
+    {
+        var datetimeBefore = DateTime.Now.AddSeconds(-1);
+        var name = "Jorge Lucas";
+        var type = CastMemberType.Director;
+
+        var castMember = new Entity.CastMember(name, type);
+
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+        castMember.Id.Should().NotBeNull();
+        castMember.Name.Should().Be(name);
+        castMember.Type.Should().Be(type);
+        (castMember.CreatedAt >= datetimeBefore).Should().BeTrue();
+        (castMember.CreatedAt <= datetimeAfter).Should().BeTrue();
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/CastMember/CastMemberTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+using FC.Codeflix.Catalog.Domain.Enum;
+using System;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.CastMember;
+
+[CollectionDefinition(nameof(CastMemberTestFixture))]
+public class CastMemberTestFixtureCollection
+    : ICollectionFixture<CastMemberTestFixture>
+{}
+
+public class CastMemberTestFixture
+    : BaseFixture
+{
+    public string GetValidName()
+        => Faker.Name.FullName();
+
+    public CastMemberType GetRandomCastMemberType()
+        => (new Random()).Next(1, 2) as CastMemberType;
+}
