2eef4694c3ce4d70fffd90d97021b1184b890842
Wilson Neto <wn_br@hotmail.com>
2022-07-03 11:03:00 -0300

    Aula 11.09: Teste do DeleteCastMember

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/DeleteCastMember/DeleteCastMemberFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Application.CastMember.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.DeleteCastMember;
+
+[CollectionDefinition(nameof(DeleteCastMemberFixture))]
+public class DeleteCastMemberFixtureCollection
+    : ICollectionFixture<DeleteCastMemberFixture>
+{ }
+
+public class DeleteCastMemberFixture
+    : CastMemberUseCasesBaseFixture
+{ }
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -0,0 +1,56 @@
+﻿using UseCase = FC.Codeflix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
+using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Repository;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.DeleteCastMember;
+
+[Collection(nameof(DeleteCastMemberFixture))]
+public class DeleteCastMemberTest
+{
+    private readonly DeleteCastMemberFixture _fixture;
+
+    public DeleteCastMemberTest(DeleteCastMemberFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteCastMember))]
+    [Trait("Application", "DeleteCastMember - Use Cases")]
+    public async Task DeleteCastMember()
+    {
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var castMemberExample = _fixture.GetExampleCastMember();
+        repositoryMock
+            .Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(castMemberExample);
+        var input = new UseCase.DeleteCastMemberInput(castMemberExample.Id);
+        var useCase = new UseCase.DeleteCastMember(
+            repositoryMock.Object, 
+            unitOfWorkMock.Object
+        );
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().NotThrowAsync();
+        repositoryMock.Verify(
+            x => x.Get(It.Is<Guid>(x => x == input.Id), It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+        repositoryMock.Verify(
+            x => x.Delete(
+                It.Is<DomainEntity.CastMember>(x => x.Id == input.Id), 
+                It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+        unitOfWorkMock.Verify(
+            x => x.Commit(It.IsAny<CancellationToken>())
+            , Times.Once
+        );
+    }
+}
