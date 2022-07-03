603ee70ac99cc5f6cb796e65ac3365c5b192d96f
Wilson Neto <wn_br@hotmail.com>
2022-07-03 08:12:14 -0300

    Aula 11.05: Testando o a validação de nome no CreateCastMember

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/CreateCastMember/CreateCastMemberTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/CreateCastMember/CreateCastMemberTest.cs
@@ -8,6 +8,7 @@ using Moq;
 using System.Threading.Tasks;
 using System.Threading;
 using FluentAssertions;
+using FC.Codeflix.Catalog.Domain.Exceptions;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.CreateCastMember;
 
@@ -24,13 +25,13 @@ public class CreateCastMemberTest
     public async Task Create()
     {
         var input = new UseCase.CreateCastMemberInput(
-            _fixture.GetValidName(), 
+            _fixture.GetValidName(),
             _fixture.GetRandomCastMemberType()
         );
         var repositoryMock = new Mock<ICastMemberRepository>();
         var unitOfWorkMock = new Mock<IUnitOfWork>();
         var useCase = new UseCase.CreateCastMember(
-            repositoryMock.Object, 
+            repositoryMock.Object,
             unitOfWorkMock.Object
         );
 
@@ -53,5 +54,30 @@ public class CreateCastMemberTest
                 It.IsAny<CancellationToken>()
             ), Times.Once
         );
+    }
+
+    [Theory(DisplayName = nameof(ThrowsWhenInvalidName))]
+    [Trait("Application", "CreateCastMember - Use Cases")]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public async Task ThrowsWhenInvalidName(string? name)
+    {
+        var input = new UseCase.CreateCastMemberInput(
+            name!,
+            _fixture.GetRandomCastMemberType()
+        );
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var useCase = new UseCase.CreateCastMember(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        var action = async () 
+            => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<EntityValidationException>()
+            .WithMessage("Name should not be empty or null");
     }
 }
