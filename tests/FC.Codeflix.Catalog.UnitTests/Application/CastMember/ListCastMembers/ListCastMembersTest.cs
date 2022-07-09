2b9f11be4920153aa41abc8e46583ea57236fdbb
Wilson Neto <wn_br@hotmail.com>
2022-07-09 13:16:09 -0300

    Aula 11.16: Testando o ListGenres

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/ListCastMembers/ListCastMembersTest.cs
@@ -0,0 +1,59 @@
+﻿using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.ListCastMembers;
+
+[Collection(nameof(ListCastMembersTestFixture))]
+public class ListCastMembersTest
+{
+    private readonly ListCastMembersTestFixture _fixture;
+
+    public ListCastMembersTest(ListCastMembersTestFixture fixture) => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCastMembers - Use Cases")]
+    public async Task List()
+    {
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var castMembersListExample = _fixture.GetExampleCastMembersList(3);
+        var repositorySearchOutput = new SearchOutput<DomainEntity.CastMember>(
+            1, 10, castMembersListExample.Count,
+            (IReadOnlyList<DomainEntity.CastMember>)castMembersListExample 
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.IsAny<SearchInput>(), It.IsAny<CancellationToken>()
+        )).ReturnsAsync(repositorySearchOutput);
+        var input = new ListCastMembersInput(1,10,"","", SearchOrder.Asc);
+        var useCase = new ListCastMembers(repositoryMock);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+        
+        output.Should().NotBeNull();
+        output.Page.Should().Be(repositorySearchOutput.CurrentPage);
+        output.PerPage.Should().Be(repositorySearchOutput.PerPage);
+        output.Total.Should().Be(repositorySearchOutput.Total);
+        output.Items.ToList().ForEach(outputItem =>
+        {
+            var example = castMembersListExample.Find(x => x.Id == outputItem.Id);
+            example.Should().NotBeNull();
+            outputItem.Name.Should().Be(example.Name);
+            outputItem.Type.Should().Be(example.Type);
+        });
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(x => (
+                x.Page == input.Page
+                && x.PerPage == input.PerPage
+                && x.Search == input.Search
+                && x.Order == input.Dir
+                && x.OrderBy == input.Order
+            )),
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CastMember/ListCastMembers/ListCastMembersTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Application.CastMember.Common;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CastMember.ListCastMembers;
+
+[CollectionDefinition(nameof(ListCastMembersTestFixture))]
+public class ListCastMembersTestFixtureCollection
+    : ICollectionFixture<ListCastMembersTestFixture>
+{ }
+
+public class ListCastMembersTestFixture
+    : CastMemberUseCasesBaseFixture
+{
+    public List<DomainEntity.CastMember> GetExampleCastMembersList(int quantity)
+        => Enumerable
+            .Range(1, quantity)
+            .Select(_ => GetExampleCastMember())
+            .ToList();
+}
