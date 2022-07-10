1df5a17ed0570afd66f46ba35959804ec8b9ab75
Wilson Neto <wn_br@hotmail.com>
2022-07-10 11:15:59 -0300

    Aula 12.02: Teste do Insert CastMemberRepository

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CastMemberRepository/CastMemberRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CastMemberRepository;
+
+[Collection(nameof(CastMemberRepositoryTestFixture))]
+public class CastMemberRepositoryTest
+{
+    private readonly CastMemberRepositoryTestFixture _fixture;
+
+    public CastMemberRepositoryTest(CastMemberRepositoryTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task Insert()
+    {
+        var castMemberExample = _fixture.GetExampleCastMember();
+        var context = _fixture.CreateDbContext();
+        var repository = new CastMemberRepository(context);
+
+        await repository.Insert(castMemberExample, CancellationToken.None);
+        context.SaveChanges();
+
+        var assertionContext = _fixture.CreateDbContext(true);
+        var castMemberFromDb = assertionContext.CastMembers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == castMemberExample.Id);
+        castMemberFromDb.Name.Should().Be(castMemberExample.Name);
+        castMemberFromDb.Type.Should().Be(castMemberExample.Type);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CastMemberRepository/CastMemberRepositoryTestFixture.cs
@@ -0,0 +1,33 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.Enum;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CastMemberRepository;
+
+[CollectionDefinition(nameof(CastMemberRepositoryTestFixture))]
+public class CastMemberRepositoryTestFixtureColletion
+    : ICollectionFixture<CastMemberRepositoryTestFixture>
+{ }
+
+public class CastMemberRepositoryTestFixture
+    : BaseFixture
+{
+    public DomainEntity.CastMember GetExampleCastMember()
+        => new(GetValidName(), GetRandomCastMemberType());
+
+    public string GetValidName()
+        => Faker.Name.FullName();
+
+    public CastMemberType GetRandomCastMemberType()
+        => (CastMemberType)(new Random()).Next(1, 2);
+
+    public List<DomainEntity.CastMember> GetExampleCastMembersList(int quantity)
+        => Enumerable
+            .Range(1, quantity)
+            .Select(_ => GetExampleCastMember())
+            .ToList();
+}
