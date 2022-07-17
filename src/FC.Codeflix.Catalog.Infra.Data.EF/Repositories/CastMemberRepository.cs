4db14e60dace8b4e0ab3d3e2c2f5220dc333fdc0
Wilson Neto <wn_br@hotmail.com>
2022-07-17 16:20:23 -0300

    Aula 12.12: Implementando o Search básico do CastMemberRepository

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CastMemberRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CastMemberRepository.cs
@@ -29,9 +29,15 @@ public class CastMemberRepository : ICastMemberRepository
         return castmember!;
     }
 
-    public Task<SearchOutput<CastMember>> Search(SearchInput input, CancellationToken cancellationToken)
+    public async Task<SearchOutput<CastMember>> Search(SearchInput input, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var items = await _castMembers.AsNoTracking().ToListAsync();
+        return new SearchOutput<CastMember>(
+            input.Page,
+            input.PerPage,
+            items.Count,
+            items.AsReadOnly()
+        );
     }
 
     public Task Update(CastMember aggregate, CancellationToken _) 
