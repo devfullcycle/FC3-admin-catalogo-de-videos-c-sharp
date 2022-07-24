dd3ea38cb93844c5aae50f669ae8b6f3f1d25456
Wilson Neto <wn_br@hotmail.com>
2022-07-24 19:28:33 -0300

    Aula 12.25": Teste de NotFound com Delete

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using FC.Codeflix.Catalog.Application.Exceptions;
 using FC.Codeflix.Catalog.Infra.Data.EF;
 using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
 using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.CastMember.Common;
@@ -37,5 +39,22 @@ public class DeleteCastMemberTest
         var assertDbContext = _fixture.CreateDbContext(true);
         var list = await assertDbContext.CastMembers.AsNoTracking().ToListAsync();
         list.Should().HaveCount(0);
+    }
+
+    [Fact(DisplayName = nameof(ThrowWhenNotFound))]
+    [Trait("Integration/Application", "DeleteCastMember - Use Cases")]
+    public async Task ThrowWhenNotFound()
+    {
+        var actDbContext = _fixture.CreateDbContext(true);
+        var repository = new CastMemberRepository(actDbContext);
+        var unitOfWork = new UnitOfWork(actDbContext);
+        var useCase = new UseCase.DeleteCastMember(repository, unitOfWork);
+        var randomGuid = Guid.NewGuid();
+        var input = new UseCase.DeleteCastMemberInput(randomGuid);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<NotFoundException>()
+            .WithMessage($"CastMember '{randomGuid}' not found.");
     }
 }
