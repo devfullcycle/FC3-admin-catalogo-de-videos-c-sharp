1bceffeee83669cbc3200cab7d1715a0d4eb1dba
Wilson Neto <wn_br@hotmail.com>
2022-08-06 20:30:32 -0300

    Aula 13.19: Teste da paginação do endpoint list

--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/CastMember/ListCastMembers/ListCastMembersApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/CastMember/ListCastMembers/ListCastMembersApiTest.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using FC.Codeflix.Catalog.Application.UseCases.CastMember.Common;
+using FC.Codeflix.Catalog.Application.UseCases.CastMember.ListCastMembers;
 using FC.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
 using FC.Codeflix.Catalog.EndToEndTests.Models;
 using FluentAssertions;
@@ -38,6 +39,45 @@ public class ListCastMembersApiTest : IDisposable
         output.Meta!.CurrentPage.Should().Be(1);
         output.Meta.Total.Should().Be(examples.Count);
         output.Data!.Should().HaveCount(examples.Count);
+        output.Data!.ForEach(outputItem =>
+        {
+            var exampleItem = examples.Find(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Id.Should().Be(exampleItem!.Id);
+            outputItem.Name.Should().Be(exampleItem.Name);
+            outputItem.Type.Should().Be(exampleItem.Type);
+        });
+    }
+
+    [Theory(DisplayName = nameof(Paginated))]
+    [Trait("EndToEnd/API", "CastMembers/List")]
+    [InlineData(10, 1, 5, 5)]
+    [InlineData(10, 2, 5, 5)]
+    [InlineData(7, 2, 5, 2)]
+    [InlineData(7, 3, 5, 0)]
+    public async Task Paginated(
+        int quantityToGenerate,
+        int page,
+        int perPage,
+        int expectedQuantityItems
+    )
+    {
+        var examples = _fixture.GetExampleCastMembersList(quantityToGenerate);
+        await _fixture.Persistence.InsertList(examples);
+
+        var (response, output) =
+            await _fixture.ApiClient.Get<TestApiResponseList<CastMemberModelOutput>>(
+                "castmembers", new ListCastMembersInput() { Page = page, PerPage = perPage}
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Meta.Should().NotBeNull();
+        output.Data.Should().NotBeNull();
+        output.Meta!.CurrentPage.Should().Be(1);
+        output.Meta.Total.Should().Be(examples.Count);
+        output.Data!.Should().HaveCount(expectedQuantityItems);
         output.Data!.ForEach(outputItem =>
         {
             var exampleItem = examples.Find(x => x.Id == outputItem.Id);
