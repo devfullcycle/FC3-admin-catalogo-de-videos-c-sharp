e631cb54ab002365e209f22270135ac784d696dd
Wilson Neto <wn_br@hotmail.com>
2022-08-06 16:35:51 -0300

    Aula 13.05: Testando o NotFound do Get CastMembers

--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/CastMember/GetCastMember/GetCastMemberApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/CastMember/GetCastMember/GetCastMemberApiTest.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using FC.Codeflix.Catalog.Api.ApiModels.Response;
 using FC.Codeflix.Catalog.Application.UseCases.CastMember.Common;
 using FC.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.EndToEndTests.Api.CastMember.GetCastMember;
@@ -25,7 +27,7 @@ public class GetCastMemberApiTest
         var example = examples[2];
         await _fixture.Persistence.InsertList(examples);
 
-        var (response, output) = 
+        var (response, output) =
             await _fixture.ApiClient.Get<ApiResponse<CastMemberModelOutput>>(
                 $"castmembers/{example.Id.ToString()}"
             );
@@ -37,5 +39,26 @@ public class GetCastMemberApiTest
         output.Data.Id.Should().Be(example.Id);
         output.Data.Name.Should().Be(example.Name);
         output.Data.Type.Should().Be(example.Type);
+    }
+
+    [Fact(DisplayName = nameof(NotFound))]
+    [Trait("EndToEnd/API", "CatMembers/Get - EndPoints")]
+    public async Task NotFound()
+    {
+        await _fixture.Persistence.InsertList(
+            _fixture.GetExampleCastMembersList(5)
+        );
+        var randomGuid = Guid.NewGuid();
+
+        var (response, output) =
+            await _fixture.ApiClient.Get<ProblemDetails>(
+                $"castmembers/{randomGuid.ToString()}"
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Title.Should().Be("Not Found");
+        output!.Detail.Should().Be($"CastMember '{randomGuid}' not found.");
     }
 }
