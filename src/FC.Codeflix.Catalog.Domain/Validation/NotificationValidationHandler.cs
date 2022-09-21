d6386db043ebf5c856b8508c79f014e05871a12a
Wilson Neto <wn_br@hotmail.com>
2022-09-21 19:23:42 -0300

    Aula 14.11: Implementando o NotificationValidationHandler

---
+++ src/FC.Codeflix.Catalog.Domain/Validation/NotificationValidationHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace FC.Codeflix.Catalog.Domain.Validation;
+public class NotificationValidationHandler : ValidationHandler
+{
+    private readonly List<ValidationError> _errors;
+    
+    public NotificationValidationHandler()
+        => _errors = new List<ValidationError>();
+    
+    public IReadOnlyCollection<ValidationError> Errors 
+        => _errors.AsReadOnly();
+
+    public bool HasErrors() => _errors.Count > 0;
+
+    public override void HandleError(ValidationError error)
+        => _errors.Add(error);
+}
