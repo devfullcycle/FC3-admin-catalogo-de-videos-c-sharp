a685fffdb7c7cb671e12a937190d297c68b69c54
Wilson Neto <wn_br@hotmail.com>
2022-09-21 16:19:19 -0300

    Aula 14.06: Setup dos testes do Agregado Video

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Video/VideoTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Video;
+
+[Collection(nameof(VideoTestFixture))]
+public class VideoTest
+{
+    private readonly VideoTestFixture _fixture;
+
+    public VideoTest(VideoTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Video - Aggregate")]
+    public void Instantiate()
+    {
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Video/VideoTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Video;
+
+[CollectionDefinition(nameof(VideoTestFixture))]
+public class VideoTestFixtureCollection : ICollectionFixture<VideoTestFixture>
+{}
+
+public class VideoTestFixture : BaseFixture
+{}
