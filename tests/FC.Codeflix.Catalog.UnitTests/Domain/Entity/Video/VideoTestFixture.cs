9d578ffb3877cf65b234260d20cea2831f1139ac
Wilson Neto <wn_br@hotmail.com>
2022-09-21 18:48:44 -0300

    Aula 14.09: Primeiro teste do Video Validator

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Video/VideoTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Video/VideoTestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using FC.Codeflix.Catalog.UnitTests.Common;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Video;
@@ -8,4 +9,13 @@ public class VideoTestFixtureCollection : ICollectionFixture<VideoTestFixture>
 {}
 
 public class VideoTestFixture : BaseFixture
-{}
+{
+    public object GetValidVideo() => new DomainEntity.Video(
+        "Title",
+        "Description",
+        2001,
+        true,
+        true,
+        180
+    );
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Video/VideoValidatorTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Video;
+
+[Collection(nameof(VideoTestFixture))]
+public class VideoValidatorTest
+{
+    private readonly VideoTestFixture _fixture;
+
+    public VideoValidatorTest(VideoTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ReturnsValidWhenVideoIsValid))]
+    [Trait("Domain", "Video Validator - Validators")]
+    public void ReturnsValidWhenVideoIsValid()
+    {
+        var validVideo = _fixture.GetValidVideo();
+        var notificationValidationHandler = new NotificationValidationHandler();
+        var videoValidator = new VideoValidator(validVideo, notificationValidationHandler);
+
+        videoValidator.Validate();
+
+        notificationValidationHandler.HasErrors().Should().BeFalse();
+        notificationValidationHandler.GetErrors().Should().HaveLength(0);
+    }
+}
