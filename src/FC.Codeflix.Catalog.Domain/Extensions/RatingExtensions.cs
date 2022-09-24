4ecbae0a1a15509233df8d4028e19442b0aa2325
Wilson Neto <wn_br@hotmail.com>
2022-09-24 19:08:39 -0300

    Aula 14.24: Criando método de conversão de string para Rating

---
+++ src/FC.Codeflix.Catalog.Domain/Extensions/RatingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FC.Codeflix.Catalog.Domain.Enum;
+
+namespace FC.Codeflix.Catalog.Domain.Extensions;
+public static class RatingExtensions
+{
+    public static Rating ToRating(this string ratingString) 
+        => ratingString switch
+    {
+        "ER" => Rating.ER,
+        "L" => Rating.L,
+        "10" => Rating.Rate10,
+        "12" => Rating.Rate12,
+        "14" => Rating.Rate14,
+        "16" => Rating.Rate16,
+        "18" => Rating.Rate18,
+        _ => throw new ArgumentOutOfRangeException(nameof(ratingString))
+    };
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Extensions/RatingExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FC.Codeflix.Catalog.Domain.Enum;
+using FC.Codeflix.Catalog.Domain.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Extensions;
+public class RatingExtensionsTest
+{
+    [Theory(DisplayName = nameof(StringToRating))]
+    [Trait("Domain", "Rating - Extensions")]
+    [InlineData("ER", Rating.ER)]
+    [InlineData("L", Rating.L)]
+    [InlineData("10", Rating.Rate10)]
+    [InlineData("12", Rating.Rate12)]
+    [InlineData("14", Rating.Rate14)]
+    [InlineData("16", Rating.Rate16)]
+    [InlineData("18", Rating.Rate18)]
+    public void StringToRating(string enumString, Rating expectedRating) 
+        => enumString.ToRating().Should().Be(expectedRating);
+
+    [Fact(DisplayName = nameof(ThrowsExceptionWhenInvalidString))]
+    [Trait("Domain", "Rating - Extensions")]
+    public void ThrowsExceptionWhenInvalidString()
+    {
+        var action = () => "Invalid".ToRating();
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}
