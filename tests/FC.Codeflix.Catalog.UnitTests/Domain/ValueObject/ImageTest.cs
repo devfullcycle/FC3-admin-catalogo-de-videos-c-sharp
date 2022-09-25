34a18b13fbc70e52670b28c51ca33e74d38e121c
Wilson Neto <wn_br@hotmail.com>
2022-09-25 13:28:34 -0300

    Aula 14.26: Escrevendo o teste para o Value Object Image

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/ValueObject/ImageTest.cs
@@ -0,0 +1,20 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+using FC.Codeflix.Catalog.Domain.ValueObject;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.ValueObject;
+
+public class ImageTest : BaseFixture
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Image - ValueObjects")]
+    public void Instantiate()
+    {
+        var path = Faker.Image.PicsumUrl();
+
+        var image = new Image(path);
+
+        image.Path.Should().Be(path);
+    }
+}
