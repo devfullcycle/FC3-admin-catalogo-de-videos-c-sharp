3cb4ae30bedf0a022ece94f1fe01ffbdfe993d18
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-06 12:31:20 -0300

    Aula 15.45: Testando a limpeza do storage no use case DeleteVideo

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTest.cs
@@ -7,6 +7,8 @@ using FC.Codeflix.Catalog.Application.Interfaces;
 using Moq;
 using System.Threading;
 using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Video.DeleteVideo;
 
@@ -17,6 +19,7 @@ public class DeleteVideoTest
     private readonly UseCase.DeleteVideo _useCase;
     private readonly Mock<IVideoRepository> _repositoryMock;
     private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IStorageService> _storageService;
 
     public DeleteVideoTest(DeleteVideoTestFixture fixture)
     {
@@ -25,13 +28,14 @@ public class DeleteVideoTest
         _unitOfWorkMock = new Mock<IUnitOfWork>();
         _useCase = new UseCase.DeleteVideo(
             _repositoryMock.Object, 
-            _unitOfWorkMock.Object
+            _unitOfWorkMock.Object,
+            _storageService.Object
         );
     }
 
     [Fact(DisplayName = nameof(DeleteVideo))]
     [Trait("Application", "DeleteVideo - Use Cases")]
-    public void DeleteVideo()
+    public async Task DeleteVideo()
     {
         var videoExample = _fixture.GetValidVideo();
         var input = _fixture.GetValidInput(videoExample.Id);
@@ -40,7 +44,7 @@ public class DeleteVideoTest
                 It.IsAny<CancellationToken>()
             )).ReturnsAsync(videoExample);
 
-        _useCase.Handle(input, CancellationToken.None);
+        await _useCase.Handle(input, CancellationToken.None);
 
         _repositoryMock.VerifyAll();
         _repositoryMock.Verify(x => x.Delete(
@@ -48,5 +52,40 @@ public class DeleteVideoTest
                 It.IsAny<CancellationToken>())
             , Times.Once);
         _unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()));
+    }
+
+    [Fact(DisplayName = nameof(DeleteVideo))]
+    [Trait("Application", "DeleteVideo - Use Cases")]
+    public async Task DeleteVideoWithAllMediasAndClearStorage()
+    {
+        var videoExample = _fixture.GetValidVideo();
+        videoExample.UpdateMedia(_fixture.GetValidMediaPath());
+        videoExample.UpdateTrailer(_fixture.GetValidMediaPath());
+        var filePaths = new List<string>() { 
+            videoExample.Media!.FilePath, 
+            videoExample.Trailer!.FilePath 
+        };
+        var input = _fixture.GetValidInput(videoExample.Id);
+        _repositoryMock.Setup(x => x.Get(
+                It.Is<Guid>(id => id == videoExample.Id),
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync(videoExample);
+
+        await _useCase.Handle(input, CancellationToken.None);
+
+        _repositoryMock.VerifyAll();
+        _repositoryMock.Verify(x => x.Delete(
+                It.Is<DomainEntity.Video>(video => video.Id == videoExample.Id),
+                It.IsAny<CancellationToken>())
+            , Times.Once);
+        _unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()));
+        _storageService.Verify(x => x.Delete(
+                It.Is<string>(filePath => filePaths.Contains(filePath)),
+                It.IsAny<CancellationToken>())
+            , Times.Exactly(2));
+        _storageService.Verify(x => x.Delete(
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>())
+            , Times.Exactly(2));
     }
 }
