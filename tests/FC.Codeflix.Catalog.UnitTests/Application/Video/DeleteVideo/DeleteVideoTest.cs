6341eecd3f921799bcb13995a1334eff78b650c5
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-06 11:58:36 -0300

    Aula 15.42: Setup para os testes do use case DeleteVideo

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTest.cs
@@ -0,0 +1,35 @@
+﻿using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.DeleteVideo;
+using FluentAssertions;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Application.Interfaces;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.DeleteVideo;
+
+[Collection(nameof(DeleteVideoTestFixture))]
+public class DeleteVideoTest
+{
+    private readonly DeleteVideoTestFixture _fixture;
+    private readonly UseCase.DeleteVideo _useCase;
+    private readonly Mock<IVideoRepository> _repositoryMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+    public DeleteVideoTest(DeleteVideoTestFixture fixture)
+    {
+        _fixture = fixture;
+        _repositoryMock = new Mock<IVideoRepository>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _useCase = new UseCase.DeleteVideo(
+            _repositoryMock.Object, 
+            _unitOfWorkMock.Object
+        );
+    }
+
+    [Fact(DisplayName = nameof(DeleteVideo))]
+    [Trait("Application", "DeleteVideo - Use Cases")]
+    public void DeleteVideo()
+    {
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.DeleteVideo;
+
+[CollectionDefinition(nameof(DeleteVideoTestFixture))]
+public class DeleteVideoTestFixtureCollection
+    : ICollectionFixture<DeleteVideoTestFixture>
+{ }
+
+public class DeleteVideoTestFixture : VideoTestFixtureBase
+{
+}
