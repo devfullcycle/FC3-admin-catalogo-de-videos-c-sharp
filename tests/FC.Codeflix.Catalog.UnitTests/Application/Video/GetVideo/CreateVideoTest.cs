60c4a748475c94a522e4e2fefcc32dab84456114
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-12 15:00:38 -0300

    Aula 15.52: Setup para os testes do use case GetVideo

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/CreateVideoTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.GetVideo;
+
+[Collection(nameof(CreateVideoTestFixture))]
+public class CreateVideoTest
+{
+    private readonly CreateVideoTestFixture _fixture;
+
+    public CreateVideoTest(CreateVideoTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Get))]
+    [Trait("Application", "GetVideo - Use Cases")]
+    public void Get()
+    {
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/CreateVideoTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.GetVideo;
+
+[CollectionDefinition(nameof(CreateVideoTestFixture))]
+public class CreateVideoTestFixtureCollection 
+    : ICollectionFixture<CreateVideoTestFixture>
+{ }
+
+public class CreateVideoTestFixture : VideoTestFixtureBase
+{ }
