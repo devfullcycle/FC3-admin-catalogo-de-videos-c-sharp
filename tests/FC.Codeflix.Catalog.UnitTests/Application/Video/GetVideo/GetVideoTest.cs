9de99070ea03311facec2829a63134286a3f84ca
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-12 15:16:54 -0300

    Aula 15.53: Testando o caminho feliz do GetVideo

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/GetVideoTest.cs
@@ -0,0 +1,46 @@
+﻿using FC.Codeflix.Catalog.Domain.Repository;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.GetVideo;
+using Moq;
+using System;
+using System.Threading;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.GetVideo;
+
+[Collection(nameof(GetVideoTestFixture))]
+public class GetVideoTest
+{
+    private readonly GetVideoTestFixture _fixture;
+
+    public GetVideoTest(GetVideoTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Get))]
+    [Trait("Application", "GetVideo - Use Cases")]
+    public async Task Get()
+    {
+        var exampleVideo = _fixture.GetValidVideo();
+        var repositoryMock = new Mock<IVideoRepository>();
+        repositoryMock.Setup(x => x.Get(
+            It.Is<Guid>(id => id == exampleVideo.Id),
+            It.IsAny<CancellationToken>())
+        ).ReturnsAsync(exampleVideo);
+        var useCase = new UseCase.GetVideo(repositoryMock.Object);
+        var input = new UseCase.GetVideoInput(exampleVideo.Id);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(exampleVideo.Id);
+        output.CreatedAt.Should().Be(exampleVideo.CreatedAt);
+        output.Title.Should().Be(exampleVideo.Title);
+        output.Published.Should().Be(exampleVideo.Published);
+        output.Description.Should().Be(exampleVideo.Description);
+        output.Duration.Should().Be(exampleVideo.Duration);
+        output.Rating.Should().Be(exampleVideo.Rating);
+        output.YearLaunched.Should().Be(exampleVideo.YearLaunched);
+        output.Opened.Should().Be(exampleVideo.Opened);
+        repositoryMock.VerifyAll();
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/GetVideoTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.GetVideo;
+
+[CollectionDefinition(nameof(GetVideoTestFixture))]
+public class GetVideoTestFixtureCollection
+    : ICollectionFixture<GetVideoTestFixture>
+{ }
+
+public class GetVideoTestFixture : VideoTestFixtureBase
+{ }
