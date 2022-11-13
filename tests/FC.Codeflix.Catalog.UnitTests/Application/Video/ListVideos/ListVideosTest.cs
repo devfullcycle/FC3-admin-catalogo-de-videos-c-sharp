f9675e7200229568862e5bec948565ec6b7e6dd5
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-13 12:30:36 -0300

    Aula 15.60: Teste do cenário feliz do ListVideos Part I

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -1,18 +1,49 @@
-﻿using Xunit;
+﻿using FC.Codeflix.Catalog.Domain.Repository;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.ListVideos;
+using DomainEntities = FC.Codeflix.Catalog.Domain.Entity;
+using Moq;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using System.Threading;
+using System.Threading.Tasks;
+using FC.Codeflix.Catalog.Application.Common;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Video.ListVideos;
 
 public class ListVideosTest
 {
     private readonly ListVideosTestFixture _fixture;
+    private readonly Mock<IVideoRepository> _videoRepositoryMock;
+    private readonly UseCase.ListVideos _useCase;
 
-    public ListVideosTest(ListVideosTestFixture fixture) 
-        => _fixture = fixture;
+    public ListVideosTest(ListVideosTestFixture fixture)
+    {
+        _fixture = fixture;
+        _videoRepositoryMock = new Mock<IVideoRepository>();
+        _useCase = new UseCase.ListVideos(_videoRepositoryMock.Object);
+    }
 
     [Fact(DisplayName = nameof(ListVideos))]
     [Trait("Application", "ListVideos - Use Cases")]
-    public void ListVideos()
+    public async Task ListVideos()
     {
+        var exampleVideosList = _fixture.CreateExampleVideosList();
+        var input = new ListVideosInput(1, 10, "", "", SearchOrder.Asc);
+        _videoRepositoryMock.Setup(x =>
+            x.Search(
+                It.Is<SearchInput>(x =>
+                    x.Page == input.Page &&
+                    x.PerPage == input.PerPage &&
+                    x.Search == input.Search &&
+                    x.OrderBy == input.Sort &&
+                    x.Order == input.Dir),
+                It.IsAny<CancellationToken>()
+            ).ReturnsAsync(exampleVideosList)
+        );
 
+        PaginatedListOutput<DomainEntities.Video> output = await _useCase.Handle(input, CancellationToken.None);
+
+        
     }
 }
+
--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
@@ -1,5 +1,9 @@
-﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+﻿using DomainEntities = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using System;
+using System.Collections.Generic;
 using Xunit;
+using System.Linq;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Video.ListVideos;
 
@@ -10,4 +14,8 @@ public class ListVideosTestFixtureCollection
 
 public class ListVideosTestFixture : VideoTestFixtureBase
 {
+    public List<DomainEntities.Video> CreateExampleVideosList() 
+        => Enumerable.Range(1, Random.Shared.Next(2, 10))
+            .Select(_ => GetValidVideoWithAllProperties())
+            .ToList();
 }
