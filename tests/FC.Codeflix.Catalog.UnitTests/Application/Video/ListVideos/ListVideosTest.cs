af423670f4e4fc3de80de0c7446b5c78f79fe34d
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-13 12:06:02 -0300

    Aula 15.59: Setup dos testes do use case ListVideos

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.ListVideos;
+
+public class ListVideosTest
+{
+    private readonly ListVideosTestFixture _fixture;
+
+    public ListVideosTest(ListVideosTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ListVideos))]
+    [Trait("Application", "ListVideos - Use Cases")]
+    public void ListVideos()
+    {
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.ListVideos;
+
+[CollectionDefinition(nameof(ListVideosTestFixture))]
+public class ListVideosTestFixtureCollection 
+    : ICollectionFixture<ListVideosTestFixture>
+{ }
+
+public class ListVideosTestFixture : VideoTestFixtureBase
+{
+}
