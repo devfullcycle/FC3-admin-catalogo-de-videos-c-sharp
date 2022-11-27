754d118424eeb33e5340564e5ad64ba51b79e6b8
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-27 16:23:44 -0300

    Aula 15.74: Testando o caso feliz do UpdateVideo com as informações básicas

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTest.cs
@@ -1,4 +1,14 @@
-﻿using Xunit;
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.UpdateVideo;
+using DomainEntities = FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using Moq;
+using Xunit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FC.Codeflix.Catalog.Application.UseCases.Video.Common;
+using FluentAssertions;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateVideo;
 
@@ -6,14 +16,54 @@ namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateVideo;
 public class UpdateVideoTest
 {
     private readonly UpdateVideoTestFixture _fixture;
+    private readonly Mock<IVideoRepository> _videoRepository;
+    private readonly Mock<IUnitOfWork> _unitofWork;
+    private readonly UseCase.UpdateVideo _useCase;
 
-    public UpdateVideoTest(UpdateVideoTestFixture fixture) 
-        => _fixture = fixture;
-
-    [Fact]
-    [Trait("Application", "UpdateVideo - Use Cases")]
-    public void UpdateVideo()
+    public UpdateVideoTest(UpdateVideoTestFixture fixture)
     {
+        _fixture = fixture;
+        _videoRepository = new();
+        _unitofWork = new();
+        _useCase = new(_videoRepository.Object, _unitofWork.Object);
+    }
 
+    [Fact(DisplayName = nameof(UpdateVideosBasicInfo))]
+    [Trait("Application", "UpdateVideo - Use Cases")]
+    public async Task UpdateVideosBasicInfo()
+    {
+        var exampleVideo = _fixture.GetValidVideo();
+        var input = _fixture.CreateValidInput(exampleVideo.Id);
+        _videoRepository.Setup(repository =>
+            repository.Get(
+                It.Is<Guid>(id => id == exampleVideo.Id),
+                It.IsAny<CancellationToken>()))
+        .ReturnsAsync(exampleVideo);
+
+        VideoModelOutput output = await _useCase.Handle(input, CancellationToken.None);
+
+        _videoRepository.VerifyAll();
+        _videoRepository.Verify(repository => repository.Update(
+            It.Is<DomainEntities.Video>(video => 
+                ((video.Id == exampleVideo.Id) &&
+                (video.Title == input.Title) && 
+                (video.Description == input.Description) &&
+                (video.Rating == input.Rating) &&
+                (video.YearLaunched == input.YearLaunched) &&
+                (video.Opened == input.Opened) &&
+                (video.Published == input.Published) &&
+                (video.Duration == input.Duration)))
+            , It.IsAny<CancellationToken>())
+        , Times.Once);
+        _unitofWork.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBe(default(DateTime));
+        output.Title.Should().Be(input.Title);
+        output.Published.Should().Be(input.Published);
+        output.Description.Should().Be(input.Description);
+        output.Duration.Should().Be(input.Duration);
+        output.Rating.Should().Be(input.Rating.ToStringSignal());
+        output.YearLaunched.Should().Be(input.YearLaunched);
+        output.Opened.Should().Be(input.Opened);
     }
 }
--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTestFixture.cs
@@ -1,4 +1,6 @@
-﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+﻿using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.UpdateVideo;
+using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using System;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateVideo;
@@ -8,4 +10,16 @@ public class UpdateVideoTestFixtureCollection : ICollectionFixture<UpdateVideoTe
 { }
 
 public class UpdateVideoTestFixture : VideoTestFixtureBase
-{ }
+{
+    public UseCase.UpdateVideoInput CreateValidInput(Guid videoId) 
+        => new(
+            videoId,
+            GetValidTitle(),
+            GetValidDescription(),
+            GetValidYearLaunched(),
+            GetRandomBoolean(),
+            GetRandomBoolean(),
+            GetValidDuration(),
+            GetRandomRating()
+        );
+}
