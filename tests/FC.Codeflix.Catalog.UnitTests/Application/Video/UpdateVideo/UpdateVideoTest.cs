fc6510bd0ef83b76a907775194271c0eaaf9e9ce
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-11-27 15:46:13 -0300

    Aula 15.73: Setup dos testes do UpdateVideo

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateVideo;
+
+[Collection(nameof(UpdateVideoTestFixture))]
+public class UpdateVideoTest
+{
+    private readonly UpdateVideoTestFixture _fixture;
+
+    public UpdateVideoTest(UpdateVideoTestFixture fixture) 
+        => _fixture = fixture;
+
+    [Fact]
+    [Trait("Application", "UpdateVideo - Use Cases")]
+    public void UpdateVideo()
+    {
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateVideo;
+
+[CollectionDefinition(nameof(UpdateVideoTestFixture))]
+public class UpdateVideoTestFixtureCollection : ICollectionFixture<UpdateVideoTestFixture>
+{ }
+
+public class UpdateVideoTestFixture : VideoTestFixtureBase
+{ }
