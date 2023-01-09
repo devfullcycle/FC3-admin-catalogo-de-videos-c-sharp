2ca174db57104655a1db5ee1ccada7f2dffcf85d
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-09 17:46:31 -0300

    Aula 16.03: Criando o teste basico do Insert

--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTest.cs
@@ -1,15 +1,57 @@
-﻿using FluentAssertions;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Enum;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
+using Repository = FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
 
 [Collection(nameof(VideoRepositoryTestFixture))]
 public class VideoRepositoryTest
 {
+    private readonly VideoRepositoryTestFixture _fixture;
+
+    public VideoRepositoryTest(VideoRepositoryTestFixture fixture) 
+        => _fixture = fixture;
+
     [Fact(DisplayName = nameof(Insert))]
     [Trait("Integration/Infra.Data", "Video Repository - Repositories")]
-    public void Insert()
+    public async Task Insert()
     {
-        1.Should().Be(1);
+        var dbContext = _fixture.CreateDbContext();
+        var exampleVideo = _fixture.GetExampleVideo();
+        IVideoRepository videoRepository = new Repository.VideoRepository(dbContext);
+
+        await videoRepository.Insert(exampleVideo, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var assertsDbContext = _fixture.CreateDbContext(true);
+        Video dbVideo = await assertsDbContext.Videos.FindAsync(exampleVideo.Id);
+        dbVideo.Should().NotBeNull();
+        dbVideo.Id.Should().Be(exampleVideo.Id);
+        dbVideo.Title.Should().Be(exampleVideo.Title);
+        dbVideo.Description.Should().Be(exampleVideo.Description);
+        dbVideo.YearLaunched.Should().Be(exampleVideo.YearLaunched);
+        dbVideo.Opened.Should().Be(exampleVideo.Opened);
+        dbVideo.Published.Should().Be(exampleVideo.Published);
+        dbVideo.Duration.Should().Be(exampleVideo.Duration);
+        dbVideo.CreatedAt.Should().BeCloseTo(exampleVideo.CreatedAt, TimeSpan.FromSeconds(1));
+        dbVideo.Rating.Should().Be(exampleVideo.Rating);
+
+        dbVideo.Thumb.Should().BeNull();
+        dbVideo.ThumbHalf.Should().BeNull();
+        dbVideo.Banner.Should().BeNull();
+        dbVideo.Media.Should().BeNull();
+        dbVideo.Trailer.Should().BeNull();
+
+        dbVideo.Genres.Should().BeEmpty();
+        dbVideo.Categories.Should().BeEmpty();
+        dbVideo.CastMembers.Should().BeEmpty();
     }
 }
--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTestFixture.cs
@@ -1,4 +1,7 @@
-﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Enum;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+using System;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
@@ -11,4 +14,43 @@ public class VideoRepositoryTestFixtureCollection
 
 public class VideoRepositoryTestFixture : BaseFixture
 {
+    public Video GetExampleVideo()
+        =>  new (
+            GetValidTitle(),
+            GetValidDescription(),
+            GetValidYearLaunched(),
+            GetRandomBoolean(),
+            GetRandomBoolean(),
+            GetValidDuration(),
+            GetRandomRating()
+        );
+
+
+    public Rating GetRandomRating()
+    {
+        var enumValue = Enum.GetValues<Rating>();
+        var random = new Random();
+        return enumValue[random.Next(enumValue.Length)];
+    }
+
+    public string GetValidTitle()
+        => Faker.Lorem.Letter(100);
+
+    public string GetValidDescription()
+        => Faker.Commerce.ProductDescription();
+
+    public string GetTooLongDescription()
+        => Faker.Lorem.Letter(4001);
+
+    public int GetValidYearLaunched()
+        => Faker.Date.BetweenDateOnly(
+            new DateOnly(1960, 1, 1),
+            new DateOnly(2022, 1, 1)
+        ).Year;
+
+    public int GetValidDuration()
+        => new Random().Next(100, 300);
+
+    public bool GetRandomBoolean()
+        => new Random().NextDouble() < 0.5;
 }
