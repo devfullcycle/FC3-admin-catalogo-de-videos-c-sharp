aca63e2fb9f96a127ec212e8be81a1c5e5d6fd1f
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-09 17:19:04 -0300

    Aula 16.02: Criando a estrutura base para os testes do VideoRepository

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
+
+[Collection(nameof(VideoRepositoryTestFixture))]
+public class VideoRepositoryTest
+{
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "Video Repository - Repositories")]
+    public void Insert()
+    {
+        1.Should().Be(1);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
+
+[CollectionDefinition(nameof(VideoRepositoryTestFixture))]
+public class VideoRepositoryTestFixtureCollection 
+    : ICollectionFixture<VideoRepositoryTestFixture>
+{
+}
+
+public class VideoRepositoryTestFixture : BaseFixture
+{
+}
