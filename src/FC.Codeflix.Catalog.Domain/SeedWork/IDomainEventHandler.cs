7b3c91998d23bf7323e4bd44c64a8eedfe9649c0
Igor Gomes <igorgomes96@hotmail.com>
2023-04-28 15:58:57 -0300

    2. Interfaces and base classes for domain events

---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/IDomainEventHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace FC.Codeflix.Catalog.Domain.SeedWork;
+public interface IDomainEventHandler<TDomainEvent> where TDomainEvent: DomainEvent
+{
+    Task Handle(TDomainEvent domainEvent, CancellationToken cancellationToken);
+}
---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/IDomainEventPublisher.cs
@@ -0,0 +1,5 @@
+﻿namespace FC.Codeflix.Catalog.Domain.SeedWork;
+public interface IDomainEventPublisher
+{
+    Task PublishAsync(DomainEvent domainEvent);
+}
