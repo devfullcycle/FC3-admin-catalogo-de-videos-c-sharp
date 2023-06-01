0a1acf498c934d6396a1bf7d24ba073423e7f2a4
Igor Gomes <igorgomes96@hotmail.com>
2023-06-01 16:20:32 -0300

    15 - ListVideos endpoint creation

--- src/FC.Codeflix.Catalog.Api/Controllers/VideosController.cs
+++ src/FC.Codeflix.Catalog.Api/Controllers/VideosController.cs
@@ -1,6 +1,8 @@
 ﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
 using FC.Codeflix.Catalog.Api.ApiModels.Video;
 using FC.Codeflix.Catalog.Application.UseCases.Video.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Video.ListVideos;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,5 +31,29 @@ public class VideosController : ControllerBase
             nameof(CreateVideo),
             new { id = output.Id },
             new ApiResponse<VideoModelOutput>(output));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ListVideosOutput), StatusCodes.Status200OK)]
+    public async Task<IActionResult> List(
+        CancellationToken cancellationToken,
+        [FromQuery] int? page = null,
+        [FromQuery(Name = "per_page")] int? perPage = null,
+        [FromQuery] string? search = null,
+        [FromQuery] string? sort = null,
+        [FromQuery] SearchOrder? dir = null
+    )
+    {
+        var input = new ListVideosInput();
+        if (page is not null) input.Page = page.Value;
+        if (perPage is not null) input.PerPage = perPage.Value;
+        if (!string.IsNullOrWhiteSpace(search)) input.Search = search;
+        if (!string.IsNullOrWhiteSpace(sort)) input.Sort = sort;
+        if (dir is not null) input.Dir = dir.Value;
+
+        var output = await _mediator.Send(input, cancellationToken);
+        return Ok(
+            new ApiResponseList<VideoModelOutput>(output)
+        );
     }
 }
