90f273428d9dab440e56d3ac1cbf2275811c6b53
Igor Gomes <igorgomes96@hotmail.com>
2023-06-26 15:54:59 -0300

    4 - UpdateMediaStatus use case unit tests

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateMediaStatus/UpdateMediaStatusTest.cs
@@ -0,0 +1,61 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Video.Common;
+using FC.Codeflix.Catalog.Domain.Enum;
+using FC.Codeflix.Catalog.Domain.Extensions;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Video.UpdateMediaStatus;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateMediaStatus;
+
+[Collection(nameof(UpdateMediaStatusTestFixture))]
+public class UpdateMediaStatusTest
+{
+    private readonly Mock<IVideoRepository> _videoRepository;
+    private readonly Mock<IUnitOfWork> _unitOfWork;
+    private readonly UseCase.UpdateMediaStatus _useCase;
+    private readonly UpdateMediaStatusTestFixture _fixture;
+
+    public UpdateMediaStatusTest(UpdateMediaStatusTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(HandleWhenSucceededEncoding))]
+    [Trait("Application", "UpdateMediaStatus - Use Cases")]
+    public async Task HandleWhenSucceededEncoding()
+    {
+        var exampleVideo = _fixture.GetValidVideoWithAllProperties();
+        var input = _fixture.GetSuccededEncodingInput(exampleVideo.Id);
+        _videoRepository.Setup(x => x.Get(
+            exampleVideo.Id,
+            It.IsAny<CancellationToken>()))
+        .ReturnsAsync(exampleVideo);
+
+        VideoModelOutput output = await _useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(exampleVideo.Id);
+        output.Title.Should().Be(exampleVideo.Title);
+        output.Description.Should().Be(exampleVideo.Description);
+        output.Published.Should().Be(exampleVideo.Published);
+        output.Opened.Should().Be(exampleVideo.Opened);
+        output.Duration.Should().Be(exampleVideo.Duration);
+        output.Rating.Should().Be(exampleVideo.Rating.ToStringSignal());
+        output.YearLaunched.Should().Be(exampleVideo.YearLaunched);
+        exampleVideo.Media!.Status.Should().Be(MediaStatus.Completed);
+        exampleVideo.Media!.EncodedPath.Should().Be(input.EncodedPath);
+        _videoRepository.VerifyAll();
+        _videoRepository.Verify(x => x.Update(
+            exampleVideo, It.IsAny<CancellationToken>()), Times.Once);
+        _unitOfWork.Setup(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/UpdateMediaStatus/UpdateMediaStatusTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using FC.Codeflix.Catalog.Domain.Enum;
+using FC.Codeflix.Catalog.UnitTests.Common.Fixtures;
+using System;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.Video.UpdateMediaStatus;
+
+[CollectionDefinition(nameof(UpdateMediaStatusTestFixture))]
+public class UpdateMediaStatusTestFixtureCollection : ICollectionFixture<UpdateMediaStatusTestFixture>
+{
+
+}
+public class UpdateMediaStatusTestFixture : VideoTestFixtureBase
+{
+    public UpdateMediaStatusInput GetSuccededEncodingInput(Guid videoId)
+        => new UpdateMediaStatusInput(
+            videoId,
+            MediaStatus.Completed,
+            EncodedPath: GetValidMediaPath());
+}
